0366872911acb55ce63207ad37f9b1d1dfa058b6
Zoechbauer <hans.zoechbauer@aon.at>
2020-08-22 09:31:59 +0200

    JUnit - assertTrue, assertFalse

--- JunitCourse/src/main/java/com/in28minutes/junit/helper/StringHelper.java
+++ JunitCourse/src/main/java/com/in28minutes/junit/helper/StringHelper.java
@@ -2,30 +2,33 @@ package com.in28minutes.junit.helper;
 
 public class StringHelper {
 
-	// AACD => CD  ACD => CD  CDEF => CDEF  CDAA => CDAA
-	public String truncateAInFirst2Positions(String str) {
-		if (str.length() <= 2)
-			return str.replaceAll("A", "");
+  // AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+  public String truncateAInFirst2Positions(String str) {
+    if (str.length() <= 2) {
+      return str.replaceAll("A", "");
+    }
 
-		String first2Chars = str.substring(0, 2);
-		String stringMinusFirst2Chars = str.substring(2);
+    final String first2Chars = str.substring(0, 2);
+    final String stringMinusFirst2Chars = str.substring(2);
 
-		return first2Chars.replaceAll("A", "") 
-				+ stringMinusFirst2Chars;
-	}
+    return first2Chars.replaceAll("A", "") + stringMinusFirst2Chars;
+  }
 
-	public boolean areFirstAndLastTwoCharactersTheSame(String str) {
+  // ABCD => false, ABAB => true, AB => true, A => false
+  public boolean areFirstAndLastTwoCharactersTheSame(String str) {
 
-		if (str.length() <= 1)
-			return false;
-		if (str.length() == 2)
-			return true;
+    if (str.length() <= 1) {
+      return false;
+    }
+    if (str.length() == 2) {
+      return true;
+    }
 
-		String first2Chars = str.substring(0, 2);
+    final String first2Chars = str.substring(0, 2);
 
-		String last2Chars = str.substring(str.length() - 2);
+    final String last2Chars = str.substring(str.length() - 2);
 
-		return first2Chars.equals(last2Chars);
-	}
+    return first2Chars.equals(last2Chars);
+  }
 
 }
--- JunitCourse/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ JunitCourse/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -1,30 +1,51 @@
 package com.in28minutes.junit.helper;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.junit.experimental.theories.internal.Assignments;
 
 public class StringHelperTest {
-	StringHelper stringHelper = new StringHelper();
-	
-	@Test
-	public void testTruncateAInFirst2Positions_AinFirst2Positions() {
-		assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
-	}
-	
-	@Test
-	public void testTruncateAInFirst2Positions_AinFirstPosition() {
-		assertEquals("CD", stringHelper.truncateAInFirst2Positions("ACD"));
-	}
-	
-	@Test
-	public void testTruncateAInFirst2Positions_noAinString() {
-		assertEquals("CDEF", stringHelper.truncateAInFirst2Positions("CDEF"));
-	}
-	
-	@Test
-	public void testTruncateAInFirst2Positions_AinEndOfString() {
-		assertEquals("CDAA", stringHelper.truncateAInFirst2Positions("CDAA"));
-	}
+  StringHelper stringHelper = new StringHelper();
+
+  @Test
+  public void testTruncateAInFirst2Positions_AinFirst2Positions() {
+    assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
+  }
+
+  @Test
+  public void testTruncateAInFirst2Positions_AinFirstPosition() {
+    assertEquals("CD", stringHelper.truncateAInFirst2Positions("ACD"));
+  }
+
+  @Test
+  public void testTruncateAInFirst2Positions_noAinString() {
+    assertEquals("CDEF", stringHelper.truncateAInFirst2Positions("CDEF"));
+  }
+
+  @Test
+  public void testTruncateAInFirst2Positions_AinEndOfString() {
+    assertEquals("CDAA", stringHelper.truncateAInFirst2Positions("CDAA"));
+  }
+
+  @Test
+  public void testAreFirstAndLastTwoCharactersTheSame_BasicNegativeScenario() {
+    assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+  }
+
+  @Test
+  public void testAreFirstAndLastTwoCharactersTheSame_BasicPositiveScenario() {
+    assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+  }
+
+  @Test
+  public void testAreFirstAndLastTwoCharactersTheSame_2Chars() {
+    assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("AB"));
+  }
+
+  @Test
+  public void testAreFirstAndLastTwoCharactersTheSame_1Char() {
+    assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("A"));
+  }
 }
