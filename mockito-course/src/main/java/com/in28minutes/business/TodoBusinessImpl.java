d43e517f73ec3c077a60fd4abe9a7b4234c39eb6
Zoechbauer <hans.zoechbauer@aon.at>
2020-08-23 12:27:38 +0200

    Mockito: Setting up SUT (System Under Test)

---
+++ mockito-course/src/main/java/com/in28minutes/business/TodoBusinessImpl.java
@@ -0,0 +1,27 @@
+package com.in28minutes.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.in28minutes.data.api.TodoService;
+
+public class TodoBusinessImpl {
+  private final TodoService todoService;
+
+  public TodoBusinessImpl(TodoService todoService) {
+    this.todoService = todoService;
+  }
+
+  public List<String> retrieveTodosRelatingToSpring(String user) {
+    final List<String> filteredTodos = new ArrayList<String>();
+    final List<String> todos = todoService.retrieveTodos(user);
+
+    for (final String todo : todos) {
+      if (todo.contains("Spring")) {
+        filteredTodos.add(todo);
+      }
+    }
+    return filteredTodos;
+  }
+
+}
