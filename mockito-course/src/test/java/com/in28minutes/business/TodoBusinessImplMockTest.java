b356d51bf2089f480c64b434af852dc66c569c81
Zoechbauer <hans.zoechbauer@aon.at>
2020-08-24 15:55:06 +0200

    Mockito: using BDD (Behavior Driven Development) with given-when-then

--- mockito-course/src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
+++ mockito-course/src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
@@ -1,6 +1,9 @@
 package com.in28minutes.business;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -40,6 +43,34 @@ public class TodoBusinessImplMockTest {
 
     final List<String> springTodos = todoBusinessImpl.retrieveTodosRelatingToSpring("DummyUser");
     assertEquals(0, springTodos.size());
+  }
+
+  @Test
+  public void testRetrieveTodosRelatingToSpring_usingBDD() {
+    final List<String> allTodos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn Unit Test");
+
+    // given
+    given(todoServiceMock.retrieveTodos("DummyUser")).willReturn(allTodos);
+
+    // when
+    final List<String> springTodos = todoBusinessImpl.retrieveTodosRelatingToSpring("DummyUser");
+
+    // then
+    assertThat(springTodos.size(), is(2));
+  }
+
+  @Test
+  public void testRetrieveTodosRelatingToSpring_emptyArray_usingBDD() {
+    final List<String> allTodos = new ArrayList<String>();
+
+    // given
+    given(todoServiceMock.retrieveTodos("DummyUser")).willReturn(allTodos);
+
+    // when
+    final List<String> springTodos = todoBusinessImpl.retrieveTodosRelatingToSpring("DummyUser");
+
+    // then
+    assertThat(springTodos.size(), is(0));
   }
 
 }
--- mockito-course/src/test/java/com/in28minutes/mockito/ListTest.java
+++ mockito-course/src/test/java/com/in28minutes/mockito/ListTest.java
@@ -1,7 +1,10 @@
 package com.in28minutes.mockito;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -9,6 +12,7 @@ import static org.mockito.Mockito.when;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ListTest {
@@ -29,11 +33,37 @@ public class ListTest {
   }
 
   @Test
+  public void LetsMockListSize_BDD() {
+    // given
+    given(mockList.size()).willReturn(2);
+
+    // when
+    final int size = mockList.size();
+
+    // then
+    assertThat(size, is(2));
+    assertThat(size, is(2));
+  }
+
+  @Test
   public void LetsMockListSize_returnMultipleValues() {
     when(mockList.size()).thenReturn(2).thenReturn(3);
 
     assertEquals(2, mockList.size());
     assertEquals(3, mockList.size());
+  }
+
+  @Test
+  @Ignore
+  public void LetsMockListSize_returnMultipleValues_BDD() {
+    given(mockList.size()).willReturn(2, 3);
+
+    // when
+    final int size = mockList.size();
+
+    // then
+    assertThat(size, is(2));
+    assertThat(size, is(3)); // actual is 2, why??
   }
 
   @Test
@@ -45,11 +75,37 @@ public class ListTest {
   }
 
   @Test
+  public void LetsMockListGet_BDD() {
+    // given
+    given(mockList.get(0)).willReturn("Hello");
+
+    // when
+    final String firstElement = (String) mockList.get(0);
+
+    // then
+    assertThat(firstElement, is("Hello"));
+  }
+
+  @Test
   public void LetsMockListGet_anyInt() {
     when(mockList.get(anyInt())).thenReturn("Same value for all integers");
 
     assertEquals("Same value for all integers", mockList.get(0));
     assertEquals("Same value for all integers", mockList.get(1));
+  }
+
+  @Test
+  public void LetsMockListGet_anyInt_BDD() {
+    // given
+    given(mockList.get(anyInt())).willReturn("Same value for all integers");
+
+    // when
+    final String firstElement = (String) mockList.get(0);
+    final String secondElement = (String) mockList.get(1);
+
+    // then
+    assertThat(firstElement, is("Same value for all integers"));
+    assertThat(secondElement, is("Same value for all integers"));
   }
 
   @Test(expected = RuntimeException.class)
