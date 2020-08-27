03e4789a144b50e444432f0fde6c4bf4fbf42917
Zoechbauer <hans.zoechbauer@aon.at>
2020-08-27 15:02:09 +0200

    Java Build Path: Changed from J2SE-1.5 to J2SE-1.8 for using Streams

--- mockito-course/src/main/java/com/in28minutes/business/TodoBusinessImpl.java
+++ mockito-course/src/main/java/com/in28minutes/business/TodoBusinessImpl.java
@@ -1,7 +1,7 @@
 package com.in28minutes.business;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.in28minutes.data.api.TodoService;
 
@@ -13,14 +13,15 @@ public class TodoBusinessImpl {
   }
 
   public List<String> retrieveTodosRelatingToSpring(String user) {
-    final List<String> filteredTodos = new ArrayList<String>();
+//    final List<String> filteredTodos = new ArrayList<String>();
+//    for (final String todo : todos) {
+//      if (todo.contains("Spring")) {
+//        filteredTodos.add(todo);
+//      }
+//    }
     final List<String> todos = todoService.retrieveTodos(user);
-
-    for (final String todo : todos) {
-      if (todo.contains("Spring")) {
-        filteredTodos.add(todo);
-      }
-    }
+    final List<String> filteredTodos = todos.stream().filter(todo -> todo.contains("Spring"))
+        .collect(Collectors.toList());
     return filteredTodos;
   }
 
